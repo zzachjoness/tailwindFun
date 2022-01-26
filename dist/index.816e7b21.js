47723205e533b39089e169d919887c6b14b4d14a
zzachjoness <zzachjoness@gmail.com>
2022-01-25 20:02:05 -0500

    hamburger fx

---
+++ dist/index.816e7b21.js
@@ -0,0 +1,16 @@
+const hamburger = document.getElementById("hamburger-button");
+const open = document.getElementById("hamburger-open");
+const close = document.getElementById("hamburger-close");
+const mobileMenu = document.getElementById("mobile-menu");
+open.addEventListener("click", ()=>{
+    open.setAttribute("class", "hidden");
+    close.setAttribute("class", "block h-6 w-6");
+    mobileMenu.setAttribute("class", "flex");
+});
+close.addEventListener("click", ()=>{
+    close.setAttribute("class", "hidden");
+    open.setAttribute("class", "block h-6 w-6");
+    mobileMenu.setAttribute("class", "hidden");
+});
+
+//# sourceMappingURL=index.816e7b21.js.map
