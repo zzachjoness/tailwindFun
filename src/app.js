b86a243740ab9767dbef0724ee2927103f6e8c0a
zzachjoness <zzachjoness@gmail.com>
2022-01-26 16:46:40 -0500

    mobile hamburger js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const mobileMenu = document.getElementById("mobile-menu");
 open.addEventListener("click", () => {
 	open.setAttribute("class", "hidden");
 	close.setAttribute("class", "block h-6 w-6");
-	mobileMenu.setAttribute("class", "flex");
+	mobileMenu.setAttribute("class", "flex sm:hidden");
 });
 close.addEventListener("click", () => {
 	close.setAttribute("class", "hidden");
