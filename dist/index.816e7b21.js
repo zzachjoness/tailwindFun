4ccd592f58b884de7ed8a7d730667dea179b0f3a
zzachjoness <zzachjoness@gmail.com>
2022-01-27 14:10:47 -0500

    hamburger overflow hidden fix

--- dist/index.816e7b21.js
+++ dist/index.816e7b21.js
@@ -2,15 +2,18 @@ const hamburger = document.getElementById("hamburger-button");
 const open = document.getElementById("hamburger-open");
 const close = document.getElementById("hamburger-close");
 const mobileMenu = document.getElementById("mobile-menu");
+const body = document.getElementById("body");
 open.addEventListener("click", ()=>{
     open.setAttribute("class", "hidden");
     close.setAttribute("class", "block h-6 w-6");
-    mobileMenu.setAttribute("class", "flex sm:hidden");
+    mobileMenu.setAttribute("class", "flex sm:hidden overflow-hidden");
+    body.setAttribute("class", "overflow-hidden");
 });
 close.addEventListener("click", ()=>{
     close.setAttribute("class", "hidden");
     open.setAttribute("class", "block h-6 w-6");
     mobileMenu.setAttribute("class", "hidden");
+    body.setAttribute("class", "");
 });
 
 //# sourceMappingURL=index.816e7b21.js.map
