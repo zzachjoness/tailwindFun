355a1a46ae3e555b4a212df259795c8910f84cca
zzachjoness <zzachjoness@gmail.com>
2022-01-27 16:53:52 -0500

    mobile menu scroll behavior

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const hamburger = document.getElementById("hamburger-button");
 const open = document.getElementById("hamburger-open");
 const close = document.getElementById("hamburger-close");
 const mobileMenu = document.getElementById("mobile-menu");
+const MobileMenuSelect = document.getElementById("mobile-menu-select");
 const body = document.getElementById("body");
 open.addEventListener("click", () => {
 	open.setAttribute("class", "hidden");
@@ -12,6 +13,10 @@ open.addEventListener("click", () => {
 close.addEventListener("click", () => {
 	close.setAttribute("class", "hidden");
 	open.setAttribute("class", "block h-6 w-6");
+	mobileMenu.setAttribute("class", "hidden");
+	body.setAttribute("class", "");
+});
+MobileMenuSelect.addEventListener("click", () => {
 	mobileMenu.setAttribute("class", "hidden");
 	body.setAttribute("class", "");
 });
