f8fdfb32aaa08b20d8735b3c34e8209f32e5c85d
zzachjoness <zzachjoness@gmail.com>
2022-02-02 16:06:51 -0500

    design section update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-	content: ["./src/index.html"],
+	content: ["./src/*.{html,js}"],
 	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-			},
-		},
+		extend: {},
 	},
 	plugins: [],
 };
