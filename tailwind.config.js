c655e84f4430fa30f1ac6763c6dfc5d0de6ab44d
zzachjoness <zzachjoness@gmail.com>
2022-02-01 22:39:46 -0500

    add home text

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-	content: ["./src/**/*.{html,js}"],
+	content: ["./src/index.html"],
 	theme: {
 		extend: {
 			fontFamily: {
